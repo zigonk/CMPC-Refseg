e9f0aa628153f02ca9407c7dbc7877554c23d9e4
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-06-21 15:10:07 +0700

    Update CMPC_model.py

--- CMPC_model.py
+++ CMPC_model.py
@@ -309,7 +309,7 @@ class LSTM_model(object):
         # Convolutional LSTM Fuse
         convlstm_cell = ConvLSTMCell([self.vf_h, self.vf_w], self.mlp_dim, [1, 1])
         convlstm_outputs, states = tf.nn.dynamic_rnn(convlstm_cell, tf.convert_to_tensor(
-            [zip(feat_exg3_2, feat_exg4_2, feat_exg5_2)]), dtype=tf.float32)
+            [list(zip(feat_exg3_2, feat_exg4_2, feat_exg5_2))]), dtype=tf.float32)
         fused_feat = convlstm_outputs[:,:,-1]
         print("Build Gated Fusion with ConvLSTM two times.")
 
