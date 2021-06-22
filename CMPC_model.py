b5fdf98a6e1b0cc36c89dc3339572cba21116d81
ZigonK <zigonk@pop-os.localdomain>
2021-06-22 15:01:11 +0700

    cast bool to int

--- CMPC_model.py
+++ CMPC_model.py
@@ -495,7 +495,7 @@ class LSTM_model(object):
         tf.summary.scalar('loss_c4', self.cls_loss_c4)
         tf.summary.scalar('loss_c5', self.cls_loss_c5)
         tf.summary.scalar('loss_last', self.cls_loss)
-        pred = tf.convert_to_tensor((self.up > 0), tf.int32)
+        pred = tf.convert_to_tensor(tf.cast(self.up > 0, tf.int32), tf.int32)
         mIOU, _ = tf.metrics.mean_iou(self.target_fine, pred, num_classes=2)
         tf.summary.scalar('mean_IOU', mIOU)
         self.merged = tf.summary.merge_all()
