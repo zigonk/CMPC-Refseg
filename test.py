be2a19e15d6ba0ad7a2398ef9f2df0ffbadf3d64
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-04 21:47:58 +0700

    Update test.py

--- test.py
+++ test.py
@@ -301,9 +301,7 @@ def test(iter, dataset, visualize, setname, dcrf, mu, tfmodel_path, model_name, 
                 # pred_raw = (scores_val >= score_thresh).astype(np.float32)
                 up_val = np.squeeze(up_val)
                 sigm_val = np.squeeze(sigm_val)
-                if (not math.isnan(consitency_score) and consitency_score < 0.5):
-                    print(consitency_score)
-                    print(vid, eid, fid)
+                if (not math.isnan(consitency_score) and consitency_score < 0.3):
                     inconsistent_frames.append((vid, eid, fid))
                 # visual_feat_c4 = np.squeeze(visual_feat_c4)
                 # break
