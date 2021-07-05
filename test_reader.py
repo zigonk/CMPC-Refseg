9b76b1e5d35a9d89b277073d6c5b00f1908b3476
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-05 18:24:27 +0700

    Update test_reader.py

--- test_reader.py
+++ test_reader.py
@@ -1,7 +1,7 @@
 from util import data_reader_refvos
 
-im_dir = '/mnt/MyPassport/Youtube-VOS/train/train/JPEGImages'
-mask_dir = '/mnt/MyPassport/Youtube-VOS/train/train/Annotations'
+im_dir = '/content/train/JPEGImages'
+mask_dir = '/content/train/Annotations'
 metadata = './train_meta.json'
 
 reader = data_reader_refvos.DataReader(im_dir, mask_dir, metadata)
