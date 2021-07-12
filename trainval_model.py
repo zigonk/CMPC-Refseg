b0c56dbd16a46f57e73f2fe1f6fef1c80635560e
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-12 13:00:05 +0700

    Update trainval_model.py

--- trainval_model.py
+++ trainval_model.py
@@ -237,7 +237,7 @@ def test(iter, dataset, visualize, setname, dcrf, mu, tfmodel_folder, model_name
         # scores_val = np.squeeze(scores_val)
         # pred_raw = (scores_val >= score_thresh).astype(np.float32)
         print('Sentence:', sent)
-        print('Words type:', words_type[0][0][len(sent) - valid_idx[0]:])
+        print('Words type:', words_type[0][0])
         up_val = np.squeeze(up_val)
         pred_raw = (up_val >= score_thresh).astype(np.float32)
         predicts = im_processing.resize_and_crop(pred_raw, mask.shape[0], mask.shape[1])
