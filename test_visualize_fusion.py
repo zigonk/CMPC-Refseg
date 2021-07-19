a7ee8cff9347a30d37381b8c68a977de47b3308b
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-07-19 22:06:03 +0700

    Update test_visualize_fusion.py

--- test_visualize_fusion.py
+++ test_visualize_fusion.py
@@ -213,7 +213,7 @@ def test(iter, dataset, visualize, setname, dcrf, mu, tfmodel_path, model_name, 
             exp = expressions[eid]['exp']
             if (eid != '2'):
                 continue
-            exp = 'a hand is toward the tree and lizard behind the white trellis'
+            exp = 'a hand'
             index = int(eid)
             vis_dir = args.visdir
 #             mask_dir = os.path.join(args.maskdir, str('{}/{}/'.format(vid, index)))
