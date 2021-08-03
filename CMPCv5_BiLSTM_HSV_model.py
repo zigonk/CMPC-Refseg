b93e1eee40871cae4f2a132d997f5c26d83a4c10
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-08-03 12:09:01 +0700

    Update CMPCv5_BiLSTM_HSV_model.py

--- CMPCv5_BiLSTM_HSV_model.py
+++ CMPCv5_BiLSTM_HSV_model.py
@@ -120,8 +120,8 @@ class LSTM_model(object):
         mu = tf.constant([104.00698793, 116.66876762, 122.67891434])
         
         im_bgr = tf.add(self.im, mu)
-        print(tf.shape(self.im))
-        print(tf.shape(im_bgr))
+        print(self.im)
+        print(im_bgr)
         im_rgb = tf.reverse(im_bgr, axis=-1)
         im_hsv = tf.image.rgb_to_hsv(im_rgb)
         visual_feat_size = tf.shape(self.visual_feat_c5)[1:3]
