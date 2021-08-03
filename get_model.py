302898a95736bb99fc15dda1e772c2b8fc77c30f
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-08-03 11:32:24 +0700

    Add color channel

--- get_model.py
+++ get_model.py
@@ -5,10 +5,11 @@ import CMPCv3_model
 import CMPCv4_model
 import CMPCv4_BiLSTM_T2_model
 import CMPCv4_BERT_model
-import CMPCv5_BiLSTM_model
 import CMPCv5_model
-
+import CMPCv5_BiLSTM_model
+import CMPCv5_BiLSTM_RGB_model
 
 def get_segmentation_model(name, **kwargs):
     model = eval(name).LSTM_model(**kwargs)
     return model
+
