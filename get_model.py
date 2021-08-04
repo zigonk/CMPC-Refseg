187754d008d423c2c227e5dfdbbbab93dfec49b5
Nguyen Quang Thuc <zthucnguyen99@gmail.com>
2021-08-04 23:02:28 +0700

    Update get_model.py

--- get_model.py
+++ get_model.py
@@ -6,9 +6,10 @@ import CMPCv4_model
 import CMPCv4_BiLSTM_T2_model
 import CMPCv4_BERT_model
 import CMPCv5_model
-import CMPCv5_HSV_model
 import CMPCv5_BiLSTM_model
 import CMPCv5_BiLSTM_HSV_model
+import CMPCv6_model
+import CMPCv6+_model
 
 def get_segmentation_model(name, **kwargs):
     model = eval(name).LSTM_model(**kwargs)
